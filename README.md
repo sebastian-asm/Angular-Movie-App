c565815820e28135df1b956a7c6f5d559ca34a88
Sebastián <sebtstian@gmail.com>
2020-11-05 20:46:53 -0300

    demo finalizado

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Angular Movie App
 
-InfiniteScroll
-Bootstrap V5
-SwiperJS
+Consumiendo API de [themoviedb.org](https://www.themoviedb.org/documentation/api) para obtener el listado de películas, aplicando Infinite Scroll, Swipper JS para los slides show y librería RxJS.
--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,6 +1,7 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { combineLatest } from 'rxjs';
 
 import { ICast } from '../../interfaces/credits-response';
 import { IMovieDetails } from '../../interfaces/movie-response';
@@ -25,16 +26,30 @@ export class MovieComponent implements OnInit {
 
   ngOnInit(): void {
     const { id } = this.activatedRoute.snapshot.params;
-    this.api.getMovieDetailsService(id).subscribe((movie) => {
-      // Cuando el id es inválido se redirecciona al home
+
+    // Suscripción a múltiples observables
+    // combineLatest recibe una cantidad "x" de observables y devuelve un array
+    combineLatest([
+      this.api.getMovieDetailsService(id),
+      this.api.getCreditsService(id),
+      // Aplicando la destructuración del array
+    ]).subscribe(([movie, cast]) => {
       if (!movie) return this.router.navigateByUrl('/home');
       this.movie = movie;
-    });
-
-    this.api.getCreditsService(id).subscribe((cast) => {
-      // Filtrar para mostrar solo actores con fotos de perfiles
       this.cats = cast.filter((actor) => actor.profile_path !== null);
     });
+
+    // Forma tradicional
+    // this.api.getMovieDetailsService(id).subscribe((movie) => {
+    //   // Cuando el id es inválido se redirecciona al home
+    //   if (!movie) return this.router.navigateByUrl('/home');
+    //   this.movie = movie;
+    // });
+
+    // this.api.getCreditsService(id).subscribe((cast) => {
+    //   // Filtrar para mostrar solo actores con fotos de perfiles
+    //   this.cats = cast.filter((actor) => actor.profile_path !== null);
+    // });
   }
 
   // Regresando de manera condicional
